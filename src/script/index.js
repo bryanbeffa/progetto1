aae471175a3af435ef0ab105020ef314d05eddaf
bryanbeffa <bryan.beffa@samtrevano.ch>
2018-10-05 16:26:58 +0200

    insignificante

--- src/script/index.js
+++ src/script/index.js
@@ -1,12 +1,24 @@
+setTimeout(centreDivMask, 3000)
+
 var signUpButton = document.getElementById("signUpButton");
 var divMask = document.getElementById("divMask");
+var preload = document.getElementById("preload");
 
-centreDivMask();
-
-//metodo che c'entra la maschera che contiene il bottone
+//metodo che c'entra la maschera che contiene il bottone e nasconde il il preload screen
 function centreDivMask(){
+	preload.style.display = "none";
+	divMask.style.display = "block";
+
 	var divMaskHeight = divMask.offsetHeight;
 	var availHeight = screen.availHeight;
 	
 	divMask.style.marginTop = (availHeight-divMaskHeight)/2 + "px";
 }
+
+//
+function fadePreloadScreen(){
+
+	centreDivMask();
+}
+
+
