fcc68e8bd0f5b708e7c8edade7b332e1fdd678bf
bryanbeffa <bryan.beffa@samtrevano.ch>
2018-10-05 17:43:14 +0200

    aggiunta animazione

--- src/script/index.js
+++ src/script/index.js
@@ -1,24 +1,46 @@
-setTimeout(centreDivMask, 3000)
-
 var signUpButton = document.getElementById("signUpButton");
 var divMask = document.getElementById("divMask");
 var preload = document.getElementById("preload");
 
+//centro la scritta benvenuto
+centerPreloadAnimation();
+centerDivMask();
+
+setTimeout(preloadAnimation, 3000)
+
 //metodo che c'entra la maschera che contiene il bottone e nasconde il il preload screen
-function centreDivMask(){
-	preload.style.display = "none";
-	divMask.style.display = "block";
-
+function centerDivMask(){
 	var divMaskHeight = divMask.offsetHeight;
-	var availHeight = screen.availHeight;
+	var windowHeight = window.innerHeight;
+	var preloadAnimationHeight = preload.offsetHeight;
 	
-	divMask.style.marginTop = (availHeight-divMaskHeight)/2 + "px";
+	divMask.style.marginTop = (windowHeight-divMaskHeight-preloadAnimationHeight)/2 + "px";
 }
 
-//
-function fadePreloadScreen(){
-
-	centreDivMask();
+//metodo che fa scomparire gradualmente la schermata di benvenuto
+function preloadAnimation(){
+	$(document).ready(function(){
+	    $("#preload").fadeOut(4000)
+	});
+	setTimeout(fadeDivMask, 4000)
 }
+
+//Metodo che fa comparire gradualmente il divMask.
+function fadeDivMask(){
+	preload.marginTop = "none";
+	$(document).ready(function(){
+	    $("#divMask").fadeIn(3000)
+	});
+}
+
+//centro il preloadscreen verticalmente
+function centerPreloadAnimation(){
+	var windowHeight = window.innerHeight;
+	var preloadAnimationHeight = preload.offsetHeight;
+
+	preload.style.marginTop = (windowHeight-preloadAnimationHeight)/2 + "px";
+}
+
+
 
 
